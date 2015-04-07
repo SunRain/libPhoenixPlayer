38dca004c1958ff459f76e3838738a78a8c102c0
SunRain <41245110@qq.com>
2015-04-07 22:45:31 +0800

    [wip] CircleImage url support

--- Plugins/QML/CircleImage.cpp
+++ Plugins/QML/CircleImage.cpp
@@ -7,6 +7,12 @@
 #include <QBrush>
 #include <QPainter>
 #include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QFile>
+
+#include "Settings.h"
+#include "Util.h"
 
 namespace PhoenixPlayer {
 namespace QmlPlugin {
@@ -15,7 +21,17 @@ CircleImage::CircleImage(QQuickPaintedItem *parent)
     : QQuickPaintedItem(parent)
 {
     mSource = QUrl();
-    //    mPixmap = 0;
+    mStatus = Status::Null;
+    mNetworkAccessManager = 0;
+    mReply = 0;
+    mImage = 0;
+    mFile = 0;
+    mRequestAborted = false;
+    mCache = true;
+    mByteArray = QByteArray();
+#ifdef SAILFISH_OS
+    mSettings = Settings::instance ();
+#endif
 }
 
 CircleImage::~CircleImage()
@@ -24,51 +40,47 @@ CircleImage::~CircleImage()
 
 void CircleImage::paint(QPainter *painter)
 {
-    if (mSource.isEmpty() || !mSource.isValid())
-        return;
-    QString str = mSource.toLocalFile();
-    if (str.isEmpty())
-        str = mSource.toString();
-    if (str.isEmpty())
-        return;
+//    if (mSource.isEmpty() || !mSource.isValid()) {
+//        this->setStatus (Status::Error);
+//        return;
+//    }
+//    QString str = mSource.toLocalFile();
+//    if (str.isEmpty())
+//        str = mSource.toString();
+//    if (str.isEmpty()) {
+//        this->setStatus (Status::Error);
+//        return;
+//    }
+//    //remove qrc: from file uri to fix image load
+//    if (str.startsWith("qrc:"))
+//        str = str.mid(3, str.length() - 3);
 
-    //remove qrc: from file uri to fix image load
-    if (str.startsWith("qrc:"))
-        str = str.mid(3, str.length() - 3);
-
-    QImage image(str);
-    if (image.isNull()) {
-        qDebug()<<"Image is null";
+//    QImage image(str);
+    if (mImage == 0 || mImage->isNull ()) {
+        qDebug()<<__FUNCTION__<<" Image is null";
+        this->setStatus (Status::Error);
         return;
     }
 
+//    this->setStatus (Status::Loading);
+
     //取得一个圆的最短长度
-    int width = image.width();
+    int width = mImage->width();
     int targetWidth = this->width();
-    int height = image.height();
+    int height = mImage->height();
     int targetHeight = this->height();
-//    if (width > targetWidth || height > targetHeight) {
-        image = image.scaled(targetWidth, targetHeight,
-                             Qt::KeepAspectRatio);
-//    }
-    width = image.width();
-    height = image.height();
+    QImage img = mImage->scaled(targetWidth, targetHeight, Qt::KeepAspectRatio);
+
+    width = img.width();
+    height = img.height();
 
     int radius = width > height ? height : width;
-
-//    qDebug()<<"image width is "<<image.width()
-//              << "image heigth is "<<image.height()
-//                 <<" this width is "<<this->width()
-//                   <<" this height is "<<this->height()
-//                 <<" radius "<<radius
-//                   <<" Draw x "<<(this->width() - radius)/2
-//                     <<" Draw y "<<(this->height() - radius)/2;
 
     //make a transparent image
     QImage out(radius, radius, QImage::Format_ARGB32_Premultiplied);
     out.fill(Qt::transparent);
 
-    QBrush b(image);
+    QBrush b(img);
     QPainter p(&out);
     p.setPen(Qt::NoPen);
     p.setRenderHint(QPainter::Antialiasing);
@@ -82,6 +94,8 @@ void CircleImage::paint(QPainter *painter)
     QPixmap pixmap  = QPixmap::fromImage(out);
     painter->setRenderHint(QPainter::SmoothPixmapTransform);
     painter->drawPixmap(boundingRect().toRect(), pixmap);
+
+    this->setStatus (Status::Ready);
 }
 
 QUrl CircleImage::source() const
@@ -91,13 +105,260 @@ QUrl CircleImage::source() const
 
 void CircleImage::setSource(const QUrl &source)
 {
-    if (source.isEmpty() || !source.isValid() || mSource == source)
+
+    qDebug()<<__FUNCTION__<<" source set to "<<source
+              <<" msource is "<<mSource;
+
+    if (mSource == source) {
         return;
+    }
     mSource = source;
+//    emit sourceChanged (mSource);
 
-//    qDebug()<<"set Source file is "<<source;
+    qDebug()<<__FUNCTION__<<" 11111111111";
+    if (mSource.isEmpty() || !mSource.isValid()) {
+        this->setStatus (Status::Error);
+        return;
+    }
+    QString str = mSource.toLocalFile();
+    if (str.isEmpty())
+        str = mSource.toString();
+    if (str.isEmpty()) {
+        this->setStatus (Status::Error);
+        return;
+    }
 
-    this->update();
+    qDebug()<<__FUNCTION__<<" 2222222222";
+
+    this->setStatus (Status::Loading);
+
+    if (str.startsWith (("http"))) { //url
+        downloadFile (mSource);
+        return;
+    }
+
+    qDebug()<<__FUNCTION__<<" 333333333333";
+
+    //remove qrc: from file uri to fix image load
+    if (str.startsWith("qrc:")) {
+        str = str.mid(3, str.length() - 3);
+    }
+    qDebug()<<__FUNCTION__<<" 44444444";
+
+    if (fillImage (str)) {
+        this->update ();
+    }
+}
+
+CircleImage::Status CircleImage::status() const
+{
+    return mStatus;
+}
+
+void CircleImage::setStatus(const CircleImage::Status &stus)
+{
+    mStatus = stus;
+    emit statusChanged (mStatus);
+}
+
+bool CircleImage::cache()
+{
+    return mCache;
+}
+
+void CircleImage::setCache(bool cache)
+{
+    mCache = cache;
+    emit cacheChanged (cache);
+}
+
+void CircleImage::downloadFile(const QUrl &url)
+{
+    qDebug()<<__FUNCTION__;
+
+    if (url.isEmpty ()) {
+        this->setStatus (Status::Error);
+        return;
+    }
+
+    QString path = mSettings->getMusicImageCachePath ();
+    QString hash = Util::calculateHash (path + url.toString ());
+    path = QString("%1/%2").arg (path).arg (hash);
+
+    ///
+    /// file exist in local cache path
+    ///
+    if (mCache) { //using image cache
+        if (QFile::exists (path)) {
+            if (fillImage (path)) { //we have the saved image data, try to load it
+                this->update ();
+                return;
+            } else { //load image data failed, remove it and download from network
+                QFile::remove (path);
+                this->setStatus (Status::Error);
+            }
+        }
+
+        if (mFile) { // constructe a new file to save image data
+            mFile->close ();
+            mFile->remove ();
+            delete  mFile;
+            mFile = 0;
+        }
+        mFile = new QFile(path);
+        if (!mFile->open (QIODevice::WriteOnly)) {
+            delete mFile;
+            mFile = 0;
+        }
+    } else {
+        if (mFile) {
+            mFile->close ();
+            delete mFile;
+            mFile = 0;
+        }
+    }
+
+    ///
+    /// download from network
+    ///
+    if (!mNetworkAccessManager)
+        mNetworkAccessManager = new QNetworkAccessManager(this);
+
+    if (mReply) {
+        mReply->abort ();
+        mRequestAborted = true;
+    }
+    if (!mByteArray.isEmpty ())
+        mByteArray.clear ();
+
+    QNetworkRequest request(url);
+    mReply = mNetworkAccessManager->get (request);
+    if (mReply) {
+        connect (mReply, &QNetworkReply::finished,
+                 this, &CircleImage::httpFinished);
+        connect (mReply, &QNetworkReply::readyRead,
+                 this, &CircleImage::httpReadyRead);
+    }
+}
+
+bool CircleImage::fillImage(const QString &imagePath)
+{
+    qDebug()<<__FUNCTION__;
+
+    if (imagePath.isEmpty ()) {
+        this->setStatus (Status::Error);
+        return false;
+    }
+    bool ret = false;
+
+    qDebug()<<__FUNCTION__<<" 111111";
+    mLock.lock ();
+    if (mImage) {
+        delete mImage;
+        mImage = 0;
+    }
+    mImage = new QImage(1,1, QImage::Format_ARGB32_Premultiplied);
+    qDebug()<<__FUNCTION__<<" 222222222";
+    if (mImage->load (imagePath)) {
+        ret = true;
+    } else {
+        if (mImage) {
+            delete mImage;
+            mImage = 0;
+        }
+        ret = false;
+    }
+    qDebug()<<__FUNCTION__<<" 333333";
+    mLock.unlock ();
+    qDebug()<<__FUNCTION__<<" load image with ret "<<ret;
+    return ret;
+}
+
+bool CircleImage::fillImage(const QByteArray &qba)
+{
+    if (qba.isEmpty ()) {
+        this->setStatus (Status::Error);
+        return false;
+    }
+    bool ret = false;
+    mLock.lock ();
+    if (mImage) {
+        delete mImage;
+        mImage = 0;
+    }
+    mImage = new QImage(1,1, QImage::Format_ARGB32_Premultiplied);
+    if (mImage->loadFromData (qba)) {
+        ret = true;
+    } else {
+        if (mImage) {
+            delete mImage;
+            mImage = 0;
+        }
+        ret = false;
+    }
+    mLock.unlock ();
+    qDebug()<<__FUNCTION__<<" load image with ret "<<ret;
+    return ret;
+}
+
+void CircleImage::httpFinished()
+{
+    if (mRequestAborted) {
+        mReply->deleteLater ();
+        mReply = 0;
+        if (mFile) {
+            mFile->close ();
+            mFile->remove ();
+            delete  mFile;
+            mFile = 0;
+        }
+        if (!mByteArray.isEmpty ())
+            mByteArray.clear ();
+        return;
+    }
+
+    if (mFile) { // if cache set to false, mFile will be 0
+        mFile->flush ();
+        mFile->close ();
+    }
+
+    if (mReply->error () != QNetworkReply::NetworkError::NoError) {
+        qDebug()<<__FUNCTION__<<" download with error "<<mReply->errorString ();
+        if (mFile) {
+            mFile->remove ();
+        }
+        mByteArray.clear ();
+    }
+    mReply->deleteLater ();
+    mReply = 0;
+    if (mFile) {
+        qDebug()<<__FUNCTION__<<"******* file name "<<mFile->fileName ();
+        if (fillImage (mFile->fileName ())) {
+            this->update ();
+        } else {
+            this->setStatus (Status::Error);
+        }
+        delete mFile;
+        mFile = 0;
+        return;
+    }
+    if (fillImage (mByteArray))
+        this->update ();
+    else
+        this->setStatus (Status::Error);
+}
+
+void CircleImage::httpReadyRead()
+{
+    // this slot gets called every time the QNetworkReply has new data.
+    // We read all of its new data and write it into the file.
+    // That way we use less RAM than when reading it at the finished()
+    // signal of the QNetworkReply
+    if (mFile) {
+        mFile->write (mReply->readAll ());
+    } else { // no local cache
+        mByteArray.append (mReply->readAll ());
+    }
 }
 
 } //QmlPlugin
--- Plugins/QML/include/CircleImage.h
+++ Plugins/QML/include/CircleImage.h
@@ -2,18 +2,32 @@
 #define CIRCLEIMAGE_H
 
 #include <QQuickPaintedItem>
+#include <QMutex>
 
 class QUrl;
-//class QPixmap;
-//class QImage;
-//class QScopedPointer;
+class QNetworkAccessManager;
+class QNetworkReply;
+class QImage;
+class QFile;
+class QByteArray;
 namespace PhoenixPlayer {
+class Settings;
 namespace QmlPlugin {
 class CircleImage : public QQuickPaintedItem
 {
     Q_OBJECT
-
+    Q_ENUMS(Status)
     Q_PROPERTY(QUrl source READ source WRITE setSource NOTIFY sourceChanged)
+    Q_PROPERTY(Status status READ status WRITE setStatus NOTIFY statusChanged)
+    Q_PROPERTY(bool cache READ cache WRITE setCache NOTIFY cacheChanged)
+public:
+    enum Status {
+        Null = 0x0,     // - no image has been set
+        Ready,          // - the image has been loaded
+        Loading,        // - the image is currently being loaded
+        Error           // - an error occurred while loading the image
+    };
+
 public:
     explicit CircleImage(QQuickPaintedItem *parent = 0);
     virtual ~CircleImage();
@@ -21,14 +35,40 @@ public:
     void paint(QPainter *painter);
 
     QUrl source() const;
+    void setSource(const QUrl &source);
+
+    Status status() const;
+    void setStatus(const Status &stus);
+
+    bool cache();
+    void setCache(bool cache);
+
 signals:
     void sourceChanged(QUrl source);
+    void statusChanged(Status status);
+    void cacheChanged(bool cache);
 public slots:
-    void setSource(const QUrl &source);
+
+private:
+//    emitStatusChange(const Status &st);
+//    void showUrlImage(const QUrl &url);
+    void downloadFile(const QUrl &url);
+    bool fillImage(const QString &imagePath);
+    bool fillImage (const QByteArray &qba);
+    void httpFinished();
+    void httpReadyRead();
 private:
     QUrl mSource;
-//    QScopedPointer<QPixmap> mPixmap;
-//    QScopedPointer<QImage> mImage;
+    Status mStatus;
+    QNetworkAccessManager *mNetworkAccessManager;
+    QNetworkReply *mReply;
+    QImage *mImage;
+    QMutex mLock;
+    QFile *mFile;
+    Settings *mSettings;
+    QByteArray mByteArray;
+    bool mRequestAborted;
+    bool mCache;
 
 };
 } //QmlPlugin
