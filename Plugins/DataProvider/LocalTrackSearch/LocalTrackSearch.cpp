27bc0a6987134633b02eb124e55bae414a93b715
SunRain <41245110@qq.com>
2020-02-19 00:29:40 +0800

    [LocalTrackSearch] skip duplication if file path contains track name (file name is same as track name)

--- Plugins/DataProvider/LocalTrackSearch/LocalTrackSearch.cpp
+++ Plugins/DataProvider/LocalTrackSearch/LocalTrackSearch.cpp
@@ -54,7 +54,7 @@ QList<MatchObject> LocalTrackSearch::match(const QString &pattern, ITrackSearch:
 
     QList<MatchObject> objList;
 
-#define DO_REG_EXP(reg_pattern, str, path, matchType) \
+#define DO_REG_EXP(reg_pattern, str, path, matchType, list) \
     QRegExp rx(reg_pattern); \
     int pos = 0; \
     while ((pos = rx.indexIn(str, pos)) != -1) { \
@@ -68,45 +68,47 @@ QList<MatchObject> LocalTrackSearch::match(const QString &pattern, ITrackSearch:
         obj.setMatchedIndex(pos); \
         obj.setMatchedLength(rx.matchedLength()); \
         obj.setMatchedStr(str); \
-        objList.append(obj); \
+        list.append(obj); \
     }
 
     AudioMetaList list = m_libMgr->allTracks();
     foreach (const auto &it, list) {
+        QList<MatchObject> tmpList;
+
+        if ((type & ITrackSearch::MatchTrackName) == ITrackSearch::MatchTrackName) {
+            const QString path = it.uri().toLocalFile();
+            const QString str = it.trackMeta().title();
+            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchTrackName, tmpList)
+        }
+
         if ((type & ITrackSearch::MatchFilePath) == ITrackSearch::MatchFilePath) {
             const QString path = it.uri().toLocalFile();
-//            QRegExp rx(str);
-//            int pos = 0;
-//            while ((pos = rx.indexIn(path, pos)) != -1) {
-//                pos += rx.matchedLength();
-//                MatchObject obj;
-//                obj.setPluginProperty(this->property());
-//                obj.setUri(path);
-//                obj.setQueryStr(str);
-//                obj.setMatchType(ITrackSearch::MatchFilePath);
-//                obj.setObjectType(MatchObject::TypeLocalFile);
-//                obj.setMatchedIndex(pos);
-//                obj.setMatchedLength(rx.matchedLength());
-//                obj.setMatchedStr(path);
-//                objList.append(obj);
-//            }
-            DO_REG_EXP(pattern, path, path, ITrackSearch::MatchFilePath)
+            // skip it file path contains track name (file name is same as track name)
+            bool skip = false;
+            foreach(const auto &node, tmpList) {
+                if (node.matchType() == ITrackSearch::MatchTrackName) {
+                    const QString tn = it.trackMeta().title();
+                    if (path.contains(tn)) {
+                        skip = true;
+                        break;
+                    }
+                }
+            }
+            if (!skip) {
+                DO_REG_EXP(pattern, path, path, ITrackSearch::MatchFilePath, tmpList)
+            }
         }
         if ((type & ITrackSearch::MatchAlbumName) == ITrackSearch::MatchAlbumName) {
             const QString path = it.uri().toLocalFile();
             const QString str = it.albumMeta().name();
-            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchAlbumName)
-        }
-        if ((type & ITrackSearch::MatchTrackName) == ITrackSearch::MatchTrackName) {
-            const QString path = it.uri().toLocalFile();
-            const QString str = it.trackMeta().title();
-            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchTrackName)
+            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchAlbumName, tmpList)
         }
         if ((type & ITrackSearch::MatchArtistName) == ITrackSearch::MatchArtistName) {
             const QString path = it.uri().toLocalFile();
             const QString str = it.artistMeta().name();
-            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchArtistName)
+            DO_REG_EXP(pattern, str, path, ITrackSearch::MatchArtistName, tmpList)
         }
+        objList.append(tmpList);
     }
     return objList;
 }
